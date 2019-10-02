d097771c750baf39716a5e7a8148804a3e44a7a0
danielbjorke <50555536+danielbjorke@users.noreply.github.com>
2019-10-02 13:59:36 +0200

    Startet på oppgave 2

--- dbj001_t5.py
+++ dbj001_t5.py
@@ -1,1 +1,30 @@
+#Oppgave1
 
+#Oppgave2
+velforening = """Tulleveien Velforening
+leder: Kari
+kasserer: Ole
+IT-ansvarlig: Liv
+parkeringsansvarlig: Kari
+arrangementsansvarlig: Liv
+hagekonsulent: Kari
+brannansvarlig: Kari
+"""
+
+def verv(navn):
+
+    alleverv = []
+    antall = 1
+
+    while antall <= velforening.count(navn):
+
+        stopp = (velforening.find(navn) - 2)
+        lenVerv = velforening[stopp:0:-1].find("\n")
+        start = stopp - lenVerv
+        ansvar = (velforening[(start + 1):stopp])
+        alleverv.append(ansvar)
+        antall += 1
+
+    print(alleverv)
+
+verv("Liv")
