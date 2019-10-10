4fd16f3f900a61f0728667be6924032e11d3509d
danielbjorke <50555536+danielbjorke@users.noreply.github.com>
2019-10-10 11:54:15 +0200

    Første utkast
    Oppgave 1 og 3 ser grei ut, men kanskje gjøre oppgave 2 litt bedre.

--- dbj001_t6.py
+++ dbj001_t6.py
@@ -1,26 +1,53 @@
-nummer = open("nummer.txt", "r+")
-backup = open("backup.txt", "w")
+#oppgave1
+def leggtil():
 
-def leggtil(fil):
+    nummer = open("telefon.txt", "a")
+
     antall = int(input("Hvor mange navn og nummer skal du legge til?\n"))
     print("Legg til navn og nummer, avlsutt med <enter>")
 
     while antall > 0:
         navnnummer = str(input("Navn og nummer:\n"))
-        fil.write("\n" + navnnummer)
+        nummer.write("\n" + navnnummer)
         antall -= 1
 
+    nummer.close()
 
-def endreNummer():
-    navn = str(input("Navn:\n"))
-    tlf = input("Nummer:\n")
+#oppgave2
+def endreTlf():
+    with open("telefon.txt") as telefon:
+        innhold = telefon.read()
 
-    for linje in nummer:
-        if navn and tlf in linje:
-            print("Ja")
+    navn = input("Navn:\n")
+
+    for plassering, ord in enumerate(innhold.split()):
+        if ord == navn.capitalize():
+            gammeltnummer = (innhold.split()[plassering+1])
+            print("Gammelt telefonnummer:", gammeltnummer)
+            nyttnummer = input("Nytt telefonnummer:\n")
+            nyttinnhold = innhold.replace(gammeltnummer, nyttnummer)
+
+            with open("telefon.txt", "w") as telefon:
+                telefon.write(nyttinnhold)
+
+#oppgave3
+def fjernVokaler(fil):
+    try:
+        vokaler = ["a", "e", "i", "o", "u", "y", "æ", "ø", "å"]
+
+        with open(fil, encoding="utf-8") as tresmå:
+            innhold = tresmå.read()
+
+        for bokstav in vokaler:
+            innhold = innhold.replace(bokstav, "")
+
+        with open("NYtreSmåKinesere.txt", "w", encoding="utf-8") as tresmå:
+            tresmå.write(innhold)
+    except:
+        print("Fant ingen fil.")
 
 
-endreNummer()
 
-nummer.close()
-backup.close()
+
+
+
